0d17ebb0c94fd8d5f5ad387c9c292e12329a9533
AldaGalarza <139023919+AldaGalarza@users.noreply.github.com>
2023-09-02 22:04:39 -0500

    Add files via upload

---
+++ ejercicio3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa el valor de numA: ");
+        int numA = scanner.nextInt();
+
+        System.out.print("Ingresa el valor de numB: ");
+        int numB = scanner.nextInt();
+
+        if (numA > 0 && numB > 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        scanner.close();
+    }
+}
+
+
+
